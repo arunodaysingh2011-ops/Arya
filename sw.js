d165bba4145b777c6aea73789d953b89103de8bd
arunodaysingh2011-ops <arunodaysingh2011@gmail.com>
2025-08-25 15:39:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,8 +7,8 @@ self.addEventListener("install", event => {
         "/style.css",
         "/script.js",
         "/manifest.json",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png"
+        "/icon-192.png",
+        "/icon-512.png"
       ]);
     })
   );
